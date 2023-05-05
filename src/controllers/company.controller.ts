401e0e5fd55b41374f4eb2ed8d4478d82ef3da34
RichardRivaldo <richardrivaldo84@gmail.com>
2023-05-06 06:10:14 +0700

    feat: added controller and service for company

---
+++ src/controllers/company.controller.ts
@@ -0,0 +1,81 @@
+import { Request, Response, Router } from "express";
+import { CompanyService } from "../services/company.service";
+
+export class CompanyController {
+    public router: Router = Router();
+    private companyService: CompanyService;
+
+    constructor(companyService: CompanyService) {
+        this.companyService = companyService;
+        this.setRoutes();
+    }
+
+    private setRoutes() {
+        this.router.get("/", this.getAll);
+        this.router.post("/", this.addCompany);
+        this.router.put("/:id/set_active", this.setActive);
+    }
+
+    private getAll = async(_: Request, res: Response) => {
+        try {
+            const result = await this.companyService.getAll();
+            res.send({
+                "status": 200,
+                "code": "200",
+                "data": {
+                    "count": result.length,
+                    "rows": result
+                },
+                "message": "Success"
+            });
+        } catch (err) {
+            res.status(500).send({
+                "status": 500,
+                "code": "500",
+                "message": "Failed retrieving company!"
+            });
+        }
+    }
+
+    private addCompany = async(req: Request, res: Response) => {
+        try {
+            const result = await this.companyService.addCompany(req.body);
+            console.log(result)
+            res.send({
+                "status": 201,
+                "code": "201",
+                "data": {
+                    "id": result._id
+                },
+                "message": "Success"
+            });
+        } catch (err) {
+            res.status(500).send({
+                "status": 500,
+                "code": "500",
+                "message": "Failed adding new company!"
+            });
+        }
+    }
+
+    private setActive = async(req: Request, res: Response) => {
+        try {
+            const result = await this.companyService.setActive(req.params.id);
+            res.send({
+                "status": 200,
+                "code": "200",
+                "data": {
+                    "id": result._id,
+                    "is_active": result.is_active
+                },
+                "message": "Success"
+            });
+        } catch (err) {
+            res.status(500).send({
+                "status": 500,
+                "code": "500",
+                "message": "Failed setting company status!"
+            });
+        }
+    }
+}
---
+++ src/services/company.service.ts
@@ -0,0 +1,23 @@
+import { ICompany } from "../interfaces/company.interface";
+import { Company } from "../models/company.models";
+
+export class CompanyService {
+    public async getAll(): Promise<ICompany[]> {
+        return Company.find({}).exec();
+    }
+
+    public async addCompany(company: ICompany): Promise<ICompany> {
+        const newCompany = new Company(company);
+        return newCompany.save();
+    }
+
+    public async setActive(id: String) {
+        const updatedCompany = await Company.findByIdAndUpdate(id, {"is_active": true}, {new: true}).exec();
+
+        if (!updatedCompany) {
+            throw new Error(`Company with id ${id} not found!`);
+        }
+        
+        return updatedCompany;
+    }
+}
